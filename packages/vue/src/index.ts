de364a0b1572e27266731ff3d40f068b0d744054
zhuzhengjian <zhuzhengjian@hoteamsoft.com>
2022-07-06 18:39:55 +0800

    feat: add vue Text

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,7 +1,14 @@
-import { Fragment, isVNode } from 'vue'
+import { Fragment, Text, isVNode } from 'vue'
 import type { VNode, VNodeChild } from 'vue'
+
 export const isFragment = (node: VNodeChild): node is VNode => {
   if (isVNode(node))
     return node.type === Fragment
+  return false
+}
+
+export const isText = (node: VNodeChild): node is VNode => {
+  if (isVNode(node))
+    return node.type === Text
   return false
 }
